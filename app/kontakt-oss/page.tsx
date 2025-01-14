b934b0ebac6b608a48c5bb7e04c8f965edb535d0
Halvor Stai Langeland <hallan24@innlandetfylke.no>
2025-01-14 09:30:29 +0100

    add log in console for contact page

--- app/kontakt-oss/page.tsx
+++ app/kontakt-oss/page.tsx
@@ -11,11 +11,22 @@ const ContactPage = () => {
 
   if (!isClient) return null;
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const fullName = (e.target as HTMLFormElement).fullName.value;
+    const email = (e.target as HTMLFormElement).email.value;
+    const details = (e.target as HTMLFormElement).details.value;
+    console.log("Skjema sendt med følgende data:");
+    console.log("Fullt Navn:", fullName);
+    console.log("Email Adresse:", email);
+    console.log("Prosjekt Detaljer:", details);
+    return false;
+  };
+
   return (
     <div className="max-w-2xl mx-auto p-10 font-lalezar">
-      <h1 className="text-5xl text-center mb-5 text-blue-500">Kontakt oss</h1>{" "}
-      {/* Increased the font size */}
-      <form className="flex flex-col gap-5">
+      <h1 className="text-5xl text-center mb-5 text-blue-500">Kontakt oss</h1>
+      <form className="flex flex-col gap-5" onSubmit={handleSubmit}>
         <div className="flex flex-col gap-2">
           <label htmlFor="fullName" className="text-gray-600">
             Fullt Navn
@@ -26,6 +37,7 @@ const ContactPage = () => {
             name="fullName"
             placeholder="Skriv ditt fulle navn"
             className="w-full p-3 border border-gray-400 rounded-md text-lg"
+            required
           />
         </div>
         <div className="flex flex-col gap-2">
@@ -38,6 +50,7 @@ const ContactPage = () => {
             name="email"
             placeholder="Skriv din e-postadresse"
             className="w-full p-3 border border-gray-400 rounded-md text-lg"
+            required
           />
         </div>
         <div className="flex flex-col gap-2 w-full">
@@ -49,6 +62,7 @@ const ContactPage = () => {
             name="details"
             placeholder="Beskriv prosjektet ditt"
             className="w-full p-3 border border-gray-400 rounded-md text-lg h-32 resize-none"
+            required
           ></textarea>
         </div>
         <button
