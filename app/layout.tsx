f913f0feffa829ef217680b95aeecabe83c1d668
Halvor Stai Langeland <hallan24@innlandetfylke.no>
2025-01-14 10:39:19 +0100

    add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,6 +36,7 @@ export default function RootLayout({
       >
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
